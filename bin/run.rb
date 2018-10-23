04a470d780118acf7dbef09bfb85f84024bcdcb0
AHCarl <ahcarlin@gmail.com>
2018-10-23 18:06:57 -0500

    refactoring, double results bug

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,5 @@
 require_relative '../config/environment'
-prompt = TTY::Prompt.new
+@prompt = TTY::Prompt.new
 
 rando_beer = Beer.find(111)
 milly = Brewery.find(858)
@@ -12,147 +12,120 @@ def spacing(user_input)
     good_input.join(' ')
 end 
 
-answer1 = prompt.select("What do you want to find?", %w(Beer Brewery))
+def ask(question)
+  @prompt.ask(question) do |q|
+    q.required true
+    q.modify :strip, :collapse
+  end
+end
+
+def more_info(found_object)
+  @prompt.select("Click to see more info:") do |options|
+    found_object.collect do |p|
+      options.choice p.name 
+    end
+  end
+end
+
+answer1 = @prompt.select("What do you want to find?", %w(Beer Brewery))
 
 if answer1 == "Beer"
-    answer2 = prompt.select("How do you want to find a beer?", %w(Name Brewery Style))
+    answer2 = @prompt.select("How do you want to find a beer?", %w(Name Brewery Style))
 
     case answer2 
     when "Name" 
-        user_input = prompt.ask("What is the name of the beer?") do |q|
-            q.required true
-            q.modify :strip, :collapse
-        end 
+        user_input = ask("What is the name of the beer?")
             
         x = Beer.find_by(name: spacing(user_input))
-        
-        prompt.select("Click to see more info:") do |options|
-            options.choice "#{x.name}", 1
-        end 
+          
         puts x.label
 
     when "Brewery"
-        user_input = prompt.ask("What is the name of the brewery?") do |q|
-            q.required true
-            q.modify :strip, :collapse
-        end 
-
+        user_input = ask("What is the name of the brewery?")
+           
         found_brewery = Brewery.find_by(name: spacing(user_input))
 
-        x= prompt.select("Click to see more info:") do |options|
+        x= @prompt.select("Click to see more info:") do |options|
             found_brewery.beers.collect do |beer|
                 options.choice beer.name
             end     
-        end 
+        end
 
-        
-         puts Beer.find_by(name: x).label
+        puts Beer.find_by(name: x).label
 
     when "Style"
-        user_input = prompt.ask("Which style?") do |q|
-            q.required true
-            q.modify :strip, :collapse
-        end 
+        user_input = ask("Which style?") 
+            
         found_by_style = Beer.find_by_style(spacing(user_input))
 
-        x= prompt.select("Click to see more info:") do |options|
-            found_by_style.collect do |beer|
-                options.choice beer.name
-            end     
-        end 
-            puts Beer.find_by(name: x).label
-
+        # x= @prompt.select("Click to see more info:") do |options|
+        #     found_by_style.collect do |beer|
+        #         options.choice beer.name
+        #     end     
+        # end 
+        x= more_info(found_by_style)
+        puts Beer.find_by(name: x).label
     end
 
 
 elsif answer1 == "Brewery"
-    answer2 = prompt.select("How do you want to find a brewery?", %w(Name City State Country))
+    answer2 = @prompt.select("How do you want to find a brewery?", %w(Name City State Country))
     case answer2
     when "Name"
-        user_input = prompt.ask("What is the name of the brewery?") do |q|
-            q.required true
-            q.modify :strip, :collapse
-        end 
-
+        user_input = ask("What is the name of the brewery?")
+            
         x = Brewery.find_by(name: spacing(user_input))
         puts x.info
 
-        prompt.keypress("Press any key to see the menu")
+        @prompt.keypress("Press any key to see the menu")
         puts x.menu
     
     when "City"
-        user_input = prompt.ask("What is the name of the city?") do |q|
-            q.required true
-            q.modify :strip, :collapse
-        end 
+        user_input = ask("What is the name of the city?")
 
-        x = Brewery.select {|b| b.city == spacing(user_input)}
-        # binding.pry
-        
-        y = prompt.select("Click to see more info:") do |options|
-                x.collect do |brewery|
+        found_breweries = Brewery.select {|b| b.city == spacing(user_input)}
 
-            options.choice brewery.name
-            end     
-         end 
+        y = more_info(found_breweries)
 
-        
-        z = x.find do |brewery|
-            brewery.name == y
-        end 
+        z = Brewery.find_by(name: y)
         
         puts z.info
-        prompt.keypress("Press any key to see the menu")
+        @prompt.keypress("Press any key to see the menu")
         puts z.menu
     
     when "State"
-        user_input = prompt.ask("What is the name of the state?") do |q|
-            q.required true
-            q.modify :strip, :collapse
-        end 
+        user_input = ask("What is the name of the state?")
+           
 
         x = Brewery.select {|b| b.state == spacing(user_input)}
-        # binding.pry
         
-        y = prompt.select("Click to see more info:") do |options|
-                x.collect do |brewery|
+        # y = @prompt.select("Click to see more info:") do |options|
+        #     x.collect do |brewery|
 
-            options.choice brewery.name
-            end     
-         end 
+        #     options.choice brewery.name
+        #     end     
+        #  end 
 
-        
-        z = x.find do |brewery|
-            brewery.name == y
-        end 
+        y = more_info(x)
+        z = Brewery.find_by(name: y)
         
         puts z.info
-        prompt.keypress("Press any key to see the menu")
+        @prompt.keypress("Press any key to see the menu")
         puts z.menu
     
     when "Country"
-        user_input = prompt.ask("What is the name of the country?") do |q|
-            q.required true
-            q.modify :strip, :collapse
-        end 
-
+        user_input = ask("What is the name of the country?")
+            
         x = Brewery.select {|b| b.country == spacing(user_input)}
-        # binding.pry
         
-        y = prompt.select("Click to see more info:") do |options|
-                x.collect do |brewery|
+        y = more_info(x) 
 
-            options.choice brewery.name
-            end     
-         end 
-
-        
         z = x.find do |brewery|
             brewery.name == y
         end 
         
         puts z.info
-        prompt.keypress("Press any key to see the menu")
+        @prompt.keypress("Press any key to see the menu")
         puts z.menu
 
     end 
