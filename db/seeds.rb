dd6dbe9ffe8ce50dd390ebc7ce61d4faa6edac53
AHCarl <ahcarlin@gmail.com>
2018-10-23 09:19:49 -0500

    additional housekeeping

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,6 @@ require 'csv'
 require 'pry'
 
 
-# def breweries_seed
-#     data = CSV.read("db/breweries.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
-#     hashed_data = data.map {|d| d.to_hash}
-# end 
-
 def breweries_seed
     CSV.foreach("db/breweries.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
         Brewery.create(row.to_hash)
@@ -16,24 +11,15 @@ end
 def styles_seed
         CSV.foreach("db/styles.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
             Style.create(row.to_hash)
-         end
+        end
     end 
 
 def beers_seed
     CSV.foreach("db/beers.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|            
         Beer.create(row.to_hash)
-end
+    end
 end 
 
-# def beers_seed
-#     data = CSV.read("db/beers.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
-#     hashed_data = data.map {|d| d.to_hash}
-# end 
-# def breweries
-#     CSV.foreach("db/breweries.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-#         Brewery.create(row.to_hash)
-#     end
-# end 
 
 breweries_seed
 styles_seed
