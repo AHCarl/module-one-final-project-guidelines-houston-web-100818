80582f8ca192f6da8bbd04dea98a8c7b3c3b3043
AHCarl <ahcarlin@gmail.com>
2018-10-24 15:10:23 -0500

    added not_found for bad searches

--- bin/run.rb
+++ bin/run.rb
@@ -35,27 +35,36 @@ def beers_select_label(all_beers)
             options.choice beer.name
         end 
     end
-
     puts Beer.find_by(name: x).label
 end
 
+def not_found 
+    puts "Sorry, we couldn't find that one."
+    start_prompt
+end
+
+def start_prompt
 answer1 = @prompt.select("What do you want to find?", %w(Beer Brewery))
 if answer1 == "Beer"
     
     answer2 = @prompt.select("How do you want to find a beer?", %w(Name Brewery Style Display\ All))
     case answer2 
     when "Name" 
+
         user_input = ask("What is the name of the beer?")
-   
+        begin
         x = Beer.find_by(name: spacing(user_input))
           
         puts x.label
+    rescue 
+        not_found
+    end
 
     when "Brewery"
         user_input = ask("What is the name of the brewery?")
-           
+        begin 
         found_brewery = Brewery.find_by(name: spacing(user_input))
-
+        
         x= @prompt.select("Click to see more info:") do |options|
             found_brewery.beers.collect do |beer|
                 options.choice beer.name
@@ -63,12 +72,17 @@ if answer1 == "Beer"
         end
         
         puts Beer.find_by(name: x).label
+    rescue 
+        not_found
+    end
         
     when "Style"
         user_input = ask("Which style?") 
-            
+        begin    
         found_by_style = Beer.find_by_style(spacing(user_input))
- 
+        rescue
+            not_found
+        end
         x= more_info(found_by_style)
         puts Beer.find_by(name: x).label
     
@@ -82,20 +96,24 @@ elsif answer1 == "Brewery"
     case answer2
     when "Name"
         user_input = ask("What is the name of the brewery?")
-            
+        begin 
         x = Brewery.find_by(name: spacing(user_input))
         puts x.info
-
+        rescue 
+            not_found
+        end
         @prompt.keypress("Press any key to see the menu")
         beers_select_label(x.beers)
     
     when "City"
         user_input = ask("What is the name of the city?")
-
+        begin
         found_breweries = Brewery.select {|b| b.city == spacing(user_input)}
 
         y = more_info(found_breweries)
-
+        rescue
+            not_found
+        end
         z = Brewery.find_by(name: y)
         
         puts z.info
@@ -105,10 +123,13 @@ elsif answer1 == "Brewery"
     when "State"
         user_input = ask("What is the name of the state?")
            
-
+        begin
         x = Brewery.select {|b| b.state == spacing(user_input)}
 
         y = more_info(x)
+        rescue
+            not_found
+        end
         z = Brewery.find_by(name: y)
         
         puts z.info
@@ -117,11 +138,14 @@ elsif answer1 == "Brewery"
     
     when "Country"
         user_input = ask("What is the name of the country?")
-            
+        
+        begin
         x = Brewery.select {|b| b.country == spacing(user_input)}
         
         y = more_info(x)
-    
+        rescue
+            not_found
+        end
         z = x.find do |brewery|
             brewery.name == y
         end 
@@ -145,3 +169,6 @@ elsif answer1 == "Brewery"
     end 
 
 end 
+end
+
+start_prompt
